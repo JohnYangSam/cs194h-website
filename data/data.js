13c48284c96781b98498e943548ee805772faca5
John Yang-Sammataro <johnys@ich.Stanford.EDU>
2017-01-10 13:53:09 -0800

    initial commit from 2016 base with minor 2017 updates

---
+++ data/data.js
@@ -0,0 +1,70 @@
+// The sole purpose of this file is to hold the data that changes from year to year. Modify the JS objects below as necessary so that Handlebars can render them properly.
+
+var projects = [
+    {
+      "link":"http://hci.stanford.edu/courses/cs194h/2016/wi/projects/buckets/",
+      "title": "Buckets",
+      "tagline": "Dream Together. Do Together.",
+      "students": ["Rohit T", "Catherine M", "Andrew F", "Raymond L"],
+    }, {
+      "link":"http://hci.stanford.edu/courses/cs194h/2016/wi/projects/clickED/",
+      "title": "clickED",
+      "tagline": "See the Questions Behind Their Masks",
+      "students": ["Gordon D", "Jake M", "Krister J", "Tyler S"]
+    }, {
+      "link":"http://hci.stanford.edu/courses/cs194h/2016/wi/projects/choreoLab/",
+      "title": "choreoLab",
+      "tagline": "Creative Conversations in Dance",
+      "students": ["Lloyd L", "Raymond C", "Sharleen T", "Rob R"],
+    }, {
+      "link":"http://hci.stanford.edu/courses/cs194h/2016/wi/projects/connect/",
+      "title": "Connect",
+      "tagline": "Making Dreams Accessible",
+      "students": ["Sage I", "Andrew M", "Vicki L", "Betsy A"]
+    }, {
+      "link":"http://hci.stanford.edu/courses/cs194h/2016/wi/projects/meet/",
+      "title": "meet",
+      "tagline": "Meetings with purpose",
+      "students": ["Derin D", "Dartis W", "Peter W", "Lorena L"],
+    }, {
+      "link":"http://hci.stanford.edu/courses/cs194h/2016/wi/projects/nightOwl/",
+      "title": "NightOwl",
+      "tagline": "Connect with classmates. Collaborate on work.",
+      "students": ["Zara S", "Griffin D", "Lachlan G", "Priyanka R", "Evan N"] 
+    }, {
+      "link":"http://hci.stanford.edu/courses/cs194h/2016/wi/projects/pause/",
+      "title": "Pause",
+      "tagline": "Encouraging the practice of self-care in a fast-paced world.",
+      "students": ["Sukhi G", "Julie N", "Raymond L", "Kyle Q"],
+    }, {
+      "link":"http://hci.stanford.edu/courses/cs194h/2016/wi/projects/petflix+chill/",
+      "title": "Petflix and Chill",
+      "tagline": "Make meaningful connections through bonding over dogs",
+      "students": ["Austin C", "Scott B", "Ken C", "Hailey S"]
+    }, {
+      "link":"http://hci.stanford.edu/courses/cs194h/2016/wi/projects/pickPlay/",
+      "title": "Jukebox",
+      "tagline": "Bringing the jukebox to the modern age.",
+      "students": ["Kyle D", "Dersu A", "Dominique Y", "Justin S"],
+    }, {
+      "link":"http://hci.stanford.edu/courses/cs194h/2016/wi/projects/spark/",
+      "title": "Spark",
+      "tagline": "Helping Teachers Help Students",
+      "students": ["Bronson D", "Lucas T", "Nathan E", "Kevin K"]
+    }, {
+      "link":"http://hci.stanford.edu/courses/cs194h/2016/wi/projects/urbanSpire/",
+      "title": "urbanSpire",
+      "tagline": "Bring what you know & love to your new community",
+      "students": ["Adrienne I", "Nikhita O", "Haley K", "Christina R"]
+    }
+];
+
+var students = [
+  {
+    link: "http://hci.stanford.edu/courses/cs194h/2017/wi/studio/foo",
+    name: "Foo A"
+  },{
+    link: "http://hci.stanford.edu/courses/cs194h/2017/wi/studio/bar",
+    name: "Bar B"
+  }
+]
